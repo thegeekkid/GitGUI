7b44192e5e5de04ffdca6e13718d373866b4b81c
thegeekkid <brian@geekkidconsulting.com>
2016-02-03 16:06:02 -0600

    Added catch for manual text input that doesn't lead to a git repository

--- GitGUI/GitGUI/Form1.vb
+++ GitGUI/GitGUI/Form1.vb
@@ -9,5 +9,11 @@
         End If
     End Sub
 
-
+    Private Sub TextBox1_TextChange_Leave(sender As Object, e As EventArgs) Handles TextBox1.Leave
+        Dim path As String = Me.TextBox1.Text
+        If Not My.Computer.FileSystem.DirectoryExists(path & "\.git") Then
+            MsgBox("Error: the folder you selected is not a GIT repository.  Please run git init and setup your repo before using this tool - this tool is only a shortcut and should not be used to replace knowing how to use GIT from the command line.")
+            Me.TextBox1.SelectAll()
+        End If
+    End Sub
 End Class
