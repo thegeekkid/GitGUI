1b0d1aad121df37394985696f43b799e08def988
thegeekkid <brian@geekkidconsulting.com>
2016-02-03 16:07:28 -0600

    Added focus call before selecting all text

--- GitGUI/GitGUI/Form1.vb
+++ GitGUI/GitGUI/Form1.vb
@@ -13,6 +13,7 @@
         Dim path As String = Me.TextBox1.Text
         If Not My.Computer.FileSystem.DirectoryExists(path & "\.git") Then
             MsgBox("Error: the folder you selected is not a GIT repository.  Please run git init and setup your repo before using this tool - this tool is only a shortcut and should not be used to replace knowing how to use GIT from the command line.")
+            Me.TextBox1.Focus()
             Me.TextBox1.SelectAll()
         End If
     End Sub
